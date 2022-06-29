a92e3cbfaef302a5331da08c61a2656c4ea20122
Shamil <karimshg@gmal.com>
2022-06-28 17:54:36 -0700

    add S2_task1

---
+++ SEMINAR2/S2_task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+void Output2(int num)
+{   
+    int num2 = (num/10)%10;
+    Console.WriteLine("второе число  " + num2);
+}
+    Console.WriteLine("Введите трехзначное число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if(num<=99 || num>999)  //если пользователь введет ошибочно не трехзначное число.
+        Console.WriteLine("ERROR");
+    else
+    {
+        Output2(num);
+    }
+    
