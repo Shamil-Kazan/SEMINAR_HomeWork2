74ec35cb1ca3c0d5ab7580669ffa177b1d1f6cef
Shamil <karimshg@gmal.com>
2022-06-28 18:05:38 -0700

    add S2_task3

---
+++ SEMINAR2/S2_task1/S2_task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+void WeekDay()
+{
+Console.WriteLine("Введите число");
+int day = new Random().Next(1, 7);
+if(day==6 || day==7)
+{
+    Console.WriteLine("Да " + day);
+}
+else
+{
+    Console.WriteLine("Нет " + day);
+}
+}
+WeekDay();
+
