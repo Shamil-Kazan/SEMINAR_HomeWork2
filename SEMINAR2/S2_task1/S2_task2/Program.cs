1854c6b33837e5e2f94a875e41cb54d8a0e0b420
Shamil <karimshg@gmal.com>
2022-06-28 18:02:07 -0700

    add S2_task2

---
+++ SEMINAR2/S2_task1/S2_task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+void ThirdNum()
+{   Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    if (num < 100)
+    {   
+        Console.WriteLine("третьей цифры нет");
+    }
+    else {
+        while (num>999)
+    {
+             num=num/10;
+    }
+    int num3 = num %10;
+    Console.WriteLine(num3);    
+    }
+}
+
+ThirdNum();
